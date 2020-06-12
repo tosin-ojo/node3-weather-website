74719f517dd71169d885cad3f8b7eb84e0554c65
Tosin Ojo <tosinojo1996@gmail.com>
2020-06-12 03:36:13 +0100

    Outlook update

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiY29kZXJ0b3NpbiIsImEiOiJja2I1a2V1cWswcW5xMnpvMmlsam1qYXpiIn0.GRqXtQWbqJeqfFZ0tv-byw&limit=1'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
