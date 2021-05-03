9e16ae0278e3a3ce9865ff2486341549219121de
Tosin Ojo <tosinojo1996@gmail.com>
2021-05-03 19:16:21 +0100

    Portfolio v-theme

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Dry',
+        title: 'Dry Me',
         name: 'BlackCoder'
     })
 })
